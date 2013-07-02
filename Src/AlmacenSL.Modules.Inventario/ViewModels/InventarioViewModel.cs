ef4d34983a4fc19b5a2ffd6a7b2c16bac98f4c9d
Manuel Guillén <isc_magg@hotmail.com>
2013-07-02 03:00:10 -0500

    Pruebas de obtención de datos desde la nueva BD Azure

--- Src/AlmacenSL.Modules.Inventario/ViewModels/InventarioViewModel.cs
+++ Src/AlmacenSL.Modules.Inventario/ViewModels/InventarioViewModel.cs
@@ -9,18 +9,41 @@ using System.Collections;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.ServiceLocation;
-
+using AlmacenSL.Web;
 
 namespace AlmacenSL.Modules.Inventario
 {
     [Export]
-    public class InventarioViewModel
+    public class InventarioViewModel : NotificationObject
     {
         [Import]
         public IRegionManager RegionManager { get; set; }
 
+        public AlmacenDomainContext context { get; set; }
+
+        ObservableCollection<User> users;
+        public ObservableCollection<User> Users
+        {
+            get { return users; }
+            set
+            {
+                users = value;
+                RaisePropertyChanged("Users");
+            }
+        }
+
         public InventarioViewModel()
-        { 
+        {
+            context = new AlmacenDomainContext();
+            getUsers();
+        }
+
+        public void getUsers()
+        {
+            context.Load(context.GetUsersQuery(), (result) => 
+                                                    {
+                                                        Users = new ObservableCollection<User>(result.Entities);
+                                                    }, null);
         }
     }
 }
